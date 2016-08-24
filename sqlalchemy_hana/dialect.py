84597774716b521ba02d459aacc12cee563eea55
Matthias Maennich <matthias.maennich@sap.com>
2016-08-24 02:46:49 +0200

    fix native boolean support

--- sqlalchemy_hana/dialect.py
+++ sqlalchemy_hana/dialect.py
@@ -49,7 +49,7 @@ class HANAStatementCompiler(compiler.SQLCompiler):
 
 class HANATypeCompiler(compiler.GenericTypeCompiler):
     def visit_boolean(self, type_):
-        return self.visit_TINYINT(type_)
+        return "BOOLEAN"
 
     def visit_NUMERIC(self, type_):
         return self.visit_DECIMAL(type_)
@@ -148,7 +148,7 @@ class HANABaseDialect(default.DefaultDialect):
     postfetch_lastrowid = False
     implicit_returning = False
     supports_empty_insert = False
-    supports_native_boolean = False
+    supports_native_boolean = True
     supports_default_values = False
     supports_sane_multi_rowcount = False
 
