a317fd081ae7242f4120de8aa80157ba62376dca
SachdevaS <33120866+SachdevaS@users.noreply.github.com>
2018-01-26 14:04:41 +0100

    Enable microsecond tests for timestamp datatype (#26)
    Still disabled with pyhdb

--- sqlalchemy_hana/requirements.py
+++ sqlalchemy_hana/requirements.py
@@ -80,8 +80,7 @@ class Requirements(requirements.SuiteRequirements):
 
     @property
     def datetime_microseconds(self):
-        """No support for microseconds in datetime"""
-        return exclusions.closed()
+        return exclusions.skip_if('hana+pyhdb')
 
     @property
     def datetime_historic(self):
