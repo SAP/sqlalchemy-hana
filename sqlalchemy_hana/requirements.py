c69fd2bea63472260060a05351305d778047f3dc
Unknown <sugandha_sachdeva@yahoo.com>
2018-01-26 14:22:57 +0100

    HANA can render limit and offset using bind parameter

--- sqlalchemy_hana/requirements.py
+++ sqlalchemy_hana/requirements.py
@@ -191,7 +191,7 @@ class Requirements(requirements.SuiteRequirements):
 
     @property
     def bound_limit_offset(self):
-        return exclusions.closed()
+        return exclusions.open()
 
     @property
     def isolation_level(self):
