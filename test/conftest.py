26a70b5945e71d12791ce289927758975b65847e
Kai Mueller <kai.mueller01@sap.com>
2023-11-09 15:40:43 +0000

    Fix

--- test/conftest.py
+++ test/conftest.py
@@ -35,6 +35,8 @@ from sqlalchemy import testing  # noqa: E402
 from sqlalchemy.testing.config import Config  # noqa: E402
 from sqlalchemy.testing.plugin.plugin_base import post  # noqa: E402
 
+TEST_SCHEMA = TEST_SCHEMA2 = ""
+
 
 def _get_main_config() -> Config:
     assert len(Config.all_configs()) == 1, "only one present config is expected"
@@ -49,15 +51,27 @@ def _random_string(length: int) -> str:
 @post
 def randomize_test_schemas(*args: Any, **kwargs: Any) -> None:
     """Set random schema names for the testing schemas."""
+    # pylint: disable=global-statement
+    global TEST_SCHEMA, TEST_SCHEMA2
+
     config = _get_main_config()
-    config.test_schema = _random_string(10)
-    config.test_schema_2 = _random_string(10)
+    config.test_schema = TEST_SCHEMA = _random_string(10)
+    config.test_schema_2 = TEST_SCHEMA2 = _random_string(10)
     Config.set_as_current(config, testing)
     testing.config.ident = _random_string(5)
 
     with config.db.connect() as connection, connection.begin():
         for schema in [config.test_schema, config.test_schema_2]:
             connection.execute(text(f"CREATE SCHEMA {schema}"))
+
+
+@pytest.fixture(autouse=True)
+def set_test_schemas() -> None:
+    """Set the right test schemas before each test."""
+    config = _get_main_config()
+    config.test_schema = TEST_SCHEMA
+    config.test_schema_2 = TEST_SCHEMA2
+    Config.set_as_current(config, testing)
 
 
 # we don't need to drop the schemas, because the user is dropped later and with it the schemas
