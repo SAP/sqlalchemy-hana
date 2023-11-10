e4f5fdd758a166a5220b274950729838a9f0a5ea
kasium <15907922+kasium@users.noreply.github.com>
2023-11-10 11:55:10 +0100

    Support parallel test session execution (#149)

--- test/conftest.py
+++ test/conftest.py
@@ -4,10 +4,15 @@
 from __future__ import annotations
 
 import logging
+import random
+import string
+from typing import Any
 
 import pytest
-from sqlalchemy import Column, Sequence, Table, event
+from sqlalchemy import Column, Sequence, Table, event, text
 from sqlalchemy.dialects import registry
+
+# isort: off
 
 logging.getLogger("sqlalchemy.engine").setLevel(logging.DEBUG)
 
@@ -22,5 +27,50 @@ def add_test_seq(column: Column, table: Table) -> None:
         column._init_items(Sequence(table.name + "_" + column.name + "_seq"))
 
 
-# enable the SQLAlchemy plugin after our setup is done
+# imports used for customizations, need to be imported AFTER SQLAlchemy plugin setup
 from sqlalchemy.testing.plugin.pytestplugin import *  # noqa: F403,F401,E402
+
+# enable the SQLAlchemy plugin after our setup is done
+from sqlalchemy import testing  # noqa: E402
+from sqlalchemy.testing.config import Config  # noqa: E402
+from sqlalchemy.testing.plugin.plugin_base import post  # noqa: E402
+
+TEST_SCHEMA = TEST_SCHEMA2 = ""
+
+
+def _get_main_config() -> Config:
+    assert len(Config.all_configs()) == 1, "only one present config is expected"
+    return list(Config.all_configs())[0]
+
+
+def _random_string(length: int) -> str:
+    """Create a random string with the given length."""
+    return "ci_" + "".join(random.choices(string.ascii_lowercase, k=length))
+
+
+@post
+def randomize_test_schemas(*args: Any, **kwargs: Any) -> None:
+    """Set random schema names for the testing schemas."""
+    # pylint: disable=global-statement
+    global TEST_SCHEMA, TEST_SCHEMA2
+
+    config = _get_main_config()
+    config.test_schema = TEST_SCHEMA = _random_string(10)
+    config.test_schema_2 = TEST_SCHEMA2 = _random_string(10)
+    Config.set_as_current(config, testing)
+    testing.config.ident = _random_string(5)
+
+    # we don't need to drop the schemas later,
+    # because the user is dropped later and with it the schemas
+    with config.db.connect() as connection, connection.begin():
+        for schema in [config.test_schema, config.test_schema_2]:
+            connection.execute(text(f"CREATE SCHEMA {schema}"))
+
+
+@pytest.fixture(autouse=True)
+def set_test_schemas() -> None:
+    """Set the right test schemas before each test."""
+    config = _get_main_config()
+    config.test_schema = TEST_SCHEMA
+    config.test_schema_2 = TEST_SCHEMA2
+    Config.set_as_current(config, testing)
