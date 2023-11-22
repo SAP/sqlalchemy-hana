af3f440ceaef8255a1cb39ab2d0fc33a1461fbdd
kasium <15907922+kasium@users.noreply.github.com>
2023-11-22 19:55:30 +0100

    Refactor test suite (#178)

--- test/test_alembic.py
+++ test/test_alembic.py
@@ -8,25 +8,26 @@ from alembic import op
 from alembic.migration import MigrationContext
 from alembic.testing.assertions import _dialect_mods
 from alembic.testing.fixtures import op_fixture
-from sqlalchemy import testing
-from sqlalchemy.testing import fixtures
+from sqlalchemy import Column
+from sqlalchemy.testing import config
+from sqlalchemy.testing.fixtures import TestBase
 
 from sqlalchemy_hana.alembic import HANAImpl
 from sqlalchemy_hana.dialect import HANAHDBCLIDialect
 
 
-class AlembicHANATest(fixtures.TestBase):
+class AlembicHANATest(TestBase):
     def test_configure_hana_migration_context(self):
         context = MigrationContext.configure(url="hana://localhost:30015")
         assert isinstance(context.dialect, HANAHDBCLIDialect)
         assert isinstance(context.impl, HANAImpl)
 
 
-class AlembicHANAOperationTest(fixtures.TestBase):
+class AlembicHANAOperationTest(TestBase):
     def setUp(self):
         class FakeModule:
             def dialect(self):
-                return testing.db.dialect
+                return config.db.dialect
 
         _dialect_mods["hana"] = FakeModule()
 
@@ -35,14 +36,14 @@ class AlembicHANAOperationTest(fixtures.TestBase):
 
     def test_add_column(self):
         context = op_fixture("hana")
-        op.add_column("some_table", sqlalchemy.Column("column_a", sqlalchemy.INTEGER))
+        op.add_column("some_table", Column("column_a", sqlalchemy.INTEGER))
         context.assert_("ALTER TABLE some_table ADD (column_a INTEGER)")
 
     def test_add_column_with_server_default(self):
         context = op_fixture("hana")
         op.add_column(
             "some_table",
-            sqlalchemy.Column(
+            Column(
                 "column_a", sqlalchemy.TIMESTAMP, server_default=sqlalchemy.func.now()
             ),
         )
@@ -105,7 +106,7 @@ class AlembicHANAOperationTest(fixtures.TestBase):
     def test_create_check_constraint(self):
         context = op_fixture("hana")
         op.create_check_constraint(
-            "ck_constraint_name", "some_table", sqlalchemy.column("age") > 0
+            "ck_constraint_name", "some_table", Column("age") > 0
         )
         context.assert_(
             """ALTER TABLE some_table ADD CONSTRAINT ck_constraint_name CHECK (age > 0)"""
